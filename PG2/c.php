a65a6b7caa571c9ca3030995f4f27f0a6fbd6c5a
evanjmcafee <139081313+evanjmcafee@users.noreply.github.com>
2024-02-17 13:25:23 -0500

    php

---
+++ PG2/c.php
@@ -0,0 +1,13 @@
+<?php
+
+$conn = pg_pconnect("dbname=publisher");
+
+// these statements will be executed as one transaction
+
+$query = "UPDATE authors SET author=UPPER(author) WHERE id=1;";
+$query .= "UPDATE authors SET author=LOWER(author) WHERE id=2;";
+$query .= "UPDATE authors SET author=NULL WHERE id=3;";
+
+pg_query($conn, $query);
+
+?>
